bfaf5464cb98e94d04dd7b05b3d21b42898d1e01
Alex Romaniuk <alaster.rom@gmail.com>
2020-03-30 13:24:55 +0300

    add line

--- AddNumbers.java
+++ AddNumbers.java
@@ -4,13 +4,22 @@ public class AddTwoNumbers {
    
    public static void main(String[] args) {
       
-      int num1 = 5;
-	  int num2 = 15; 
-	  int sum;
+      int num1 = 5, int num2 = 15, int sum;
 
       sum = num1 + num2;
       
       logger.log("Sum of these numbers: " + sum);
+   }
+   
+   public static void main(String[] args) {
+      
+      int num1 = 3;
+	  int num2 = 14;
+	  int dif;
+
+      del = num1 - num2;
+	  
+      logger.log("Dif of these numbers: " + dif);
    }
 
    public static void main(String[] args) {
