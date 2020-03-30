3807449eb24c6c6e0afc9cb8de921e7744919747
Alex Romaniuk <alaster.rom@gmail.com>
2020-03-30 13:20:03 +0300

    add

---
+++ main/java/AddTwoNumbers.java
@@ -0,0 +1,26 @@
+package src;
+
+public class AddTwoNumbers {
+   
+   public static void main(String[] args) {
+      
+      int num1 = 5;
+	  int num2 = 15;
+	  int sum;
+
+      sum = num1 + num2;
+      
+      logger.log("Sum of these numbers: " + sum);
+   }
+   
+   public static void main(String[] args) {
+      
+      int num1 = 5;
+	  int num2 = 15;
+	  int del;
+
+      del = num1 / num2;
+	  
+      logger.log("Sum of these numbers: " + del);
+   }
+}
