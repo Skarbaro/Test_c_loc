b9bdc63f487e10fae48ebb31caca76303ac157cc
Alex Romaniuk <alaster.rom@gmail.com>
2020-05-28 16:09:18 +0300

    change file 1 java / br6

--- main/java/Client_Window.java
+++ main/java/Client_Window.java
@@ -4,17 +4,11 @@ public class AddTwoNumbers {
 	private String color;
 	
 	public static void main(String[] args) {
-		int num1 = 5, num2 = 15, sum;
+		int num1 = 7, num2 = 15, sum;
 		sum = num1 + num2;
 		System.out.println("Sum of these numbers: " + sum);
 	}
-	
-	public static void main(String[] args) {
-		int num1 = 5, num2 = 15, del;
-		del = num1 + num2;
-		System.out.println("Del of these numbers: " + del);
-	}
-	
+
 	public static void main(String[] args) {
 		int num1 = 5, num2 = 15, del;
 		del = num1 + num2;
